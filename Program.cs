9cd6b6a1cc53fc4d28345d3c7f181d2c03de8de9
Garun Vagidov <garun@garunski.com>
2023-04-12 14:26:47 -0500

    attempt 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Pulumi;
 using Pulumi.DigitalOcean;
+using Pulumi.DigitalOcean.Inputs;
+using Config = Pulumi.Config;
 
-return await Pulumi.Deployment.RunAsync(() =>
+return await Deployment.RunAsync(() =>
 {
+    var config = new Config();
+
+    var stackName = Deployment.Instance.StackName;
+
     var domain = new Domain("scry-root", new()
     {
-        Name = "scryapp.website",
+        Name = "scryapp.website"
     });
 
     var local = new DnsRecord("local", new()
@@ -13,7 +19,7 @@ return await Pulumi.Deployment.RunAsync(() =>
         Domain = domain.Id,
         Name = "local",
         Type = "A",
-        Value = "192.168.1.200",
+        Value = "192.168.1.200"
     });
 
     var wildCardLocal = new DnsRecord("wildcardlocal", new()
@@ -21,6 +27,36 @@ return await Pulumi.Deployment.RunAsync(() =>
         Domain = domain.Id,
         Name = "*.local",
         Type = "A",
-        Value = "192.168.1.200",
+        Value = "192.168.1.200"
+    });
+
+    var vpc = new Vpc("vpc", new VpcArgs
+    {
+        Name = stackName + "-vpc",
+        Region = "nyc1",
+        IpRange = "10.0.0.0/16"
+    });
+
+    var dbCluster = new DatabaseCluster(stackName + "-db-cluster", new DatabaseClusterArgs
+    {
+        Name = stackName + "-pg-cluster",
+        Engine = "pg",
+        Region = "nyc1",
+        Size = "db-s-1vcpu-1gb",
+        Version = "15",
+        PrivateNetworkUuid = vpc.Id
+    });
+
+    var dbFirewall = new DatabaseFirewall(stackName + "-db-firewall", new DatabaseFirewallArgs
+    {
+        ClusterId = dbCluster.Id,
+        Rules = new InputList<DatabaseFirewallRuleArgs>
+        {
+            new DatabaseFirewallRuleArgs
+            {
+                Type = "vpc",
+                Value = vpc.Id
+            }
+        }
     });
 });
